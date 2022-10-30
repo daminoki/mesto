4e7c9d282d83fbebe14ad680785c13aaf2df3fc4
Daria Andreeva <dd1556@mail.ru>
2022-10-30 21:36:03 +0300

    edit form fixed

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const editButton = document.querySelector('.profile-info__edit-button');
+const popup = document.querySelector('.popup');
+const userName = document.querySelector('.profile-info__user-name');
+const userNameInput = document.querySelector('.popup__input_name');
+const userDescription = document.querySelector('.profile-info__user-description');
+const userDescriptionInput = document.querySelector('.popup__input_about');
 
 function openEditForm() {
-    let popup = document.querySelector('.popup');
     popup.classList.add('popup_opened');
+    userNameInput.value = userName.textContent;
+    userDescriptionInput.value = userDescription.textContent;
 }
 
 editButton.addEventListener('click', openEditForm);
@@ -10,8 +16,26 @@ editButton.addEventListener('click', openEditForm);
 const closeButton = document.querySelector('.popup__close-button');
 
 function closeEditForm () {
-    let popup = document.querySelector('.popup');
     popup.classList.remove('popup_opened');
 }
 
 closeButton.addEventListener('click', closeEditForm);
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    userName.textContent = userNameInput.value;
+    userDescription.textContent = userDescriptionInput.value;
+    closeEditForm();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
