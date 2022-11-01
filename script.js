3a3344dbcb77c108157fd31d34e1e38bf63e170c
Dasha Andreeva <dd1556@mail.ru>
2022-11-01 18:05:53 +0300

    comments deleted

--- script.js
+++ script.js
@@ -26,21 +26,17 @@ window.addEventListener('keyup', function (evt) {
     }
 });
 
-// Находим форму в DOM
+
 let formElement = document.querySelector('.popup__form');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+    evt.preventDefault(); 
 
     userName.textContent = userNameInput.value;
     userDescription.textContent = userDescriptionInput.value;
     closeEditForm();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+
 formElement.addEventListener('submit', formSubmitHandler); 
