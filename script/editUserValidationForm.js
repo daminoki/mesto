a79237f41d81c02aa79febb5d1ec35dda1c9aa65
Daria Andreeva <dd1556@mail.ru>
2022-11-21 09:48:06 +0300

    enableValidationForm changes

--- script/editUserValidationForm.js
+++ script/editUserValidationForm.js
@@ -5,3 +5,38 @@ const showInputError = (formElement, inputElement, errorMessage) => {
     errorElement.classList.add("popup__input_error-message_active");
 };
 
+const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(".${inputElement-id}-error");
+    inputElement.classList.remove("popup__input_error");
+    errorElement.classList.remove("popup__input_error-message_active");
+    errorElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+        hideInputError(formElement, inputElement);
+    }
+};
+
+const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener("input", function() {
+            checkInputValidity(formElement, inputElement);
+        });
+    });
+};
+
+const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll(".popup__form"));
+    formList.forEach((formElement) => {
+        formElement.addEventListener("submit", (evt) => {
+            evt.preventDefault();
+        });
+        setEventListeners(formElement);
+});
+};
+
+enableValidation();
